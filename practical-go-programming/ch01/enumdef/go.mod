6f272bd45aa04c9cb86f60f486f1e482332a3d75
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2022-06-19 08:29:25 +0900

    add

---
+++ practical-go-programming/ch01/enumdef/go.mod
@@ -0,0 +1,3 @@
+module github.com/n-kurasawa/go-sandbox/practical-go-programming/ch01/enumdef
+
+go 1.17
--- practical-go-programming/ch09/main.go
+++ practical-go-programming/ch09/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -22,10 +23,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	s := &Service{db: db}
-	user := User{UserID: "2", UserName: "test2", CreatedAt: time.Now()}
-	if err := s.CreateUser(ctx, user); err != nil {
-		fmt.Println(err.Error())
+	s := &Service{tx: txAdmin{db}}
+	if err := s.UpdateProduct(ctx); err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -35,24 +35,39 @@ type User struct {
 	CreatedAt time.Time
 }
 
-type Service struct {
-	db *sql.DB
+// transaction-wrapper-start
+// txAdminはトランザクション制御するための構造体
+type txAdmin struct {
+	*sql.DB
 }
 
-func (s *Service) CreateUser(ctx context.Context, user User) (err error) {
-	tx, err := s.db.Begin()
+type Service struct {
+	tx txAdmin
+}
+
+// Transaction はトランザクションを制御するメソッド
+// アプリケーション開発者が本メソッドを使って、DMLのクエリーを発行する
+func (t *txAdmin) Transaction(ctx context.Context, f func(ctx context.Context, tx *sql.Tx) (err error)) error {
+	tx, err := t.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
-	defer func() {
-		if err := tx.Rollback(); err != nil {
-			fmt.Println(err.Error())
-		}
-	}()
-
-	if _, err = tx.ExecContext(ctx, `INSERT INTO users (user_id, user_name, created_at) VALUES ($1, $2, $3)`, user.UserID, user.UserName, user.CreatedAt); err != nil {
-		return err
+	defer tx.Rollback()
+	if err := f(ctx, tx); err != nil {
+		return fmt.Errorf("transaction query failed: %w", err)
 	}
-
 	return tx.Commit()
+}
+
+func (s *Service) UpdateProduct(ctx context.Context) error {
+	updateFunc := func(ctx context.Context, tx *sql.Tx) error {
+		if _, err := s.tx.ExecContext(ctx, `UPDATE users SET user_name = 'upd123' WHERE user_id = '1';`); err != nil {
+			return err
+		}
+		if _, err := s.tx.ExecContext(ctx, `UPDATE users SET user_name = 'upd234' WHERE user_id = '2';`); err != nil {
+			return err
+		}
+		return errors.New("error")
+	}
+	return s.tx.Transaction(ctx, updateFunc)
 }
