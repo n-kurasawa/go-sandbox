b7d09ef473faaccc12e9868047c0363adccb6cee
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2022-06-29 09:26:39 +0900

    add Analyzer

--- cohesion/main.go
+++ cohesion/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/singlechecker"
+)
+
+var analyzer = &analysis.Analyzer{
+	Name: "cohesion",
+	Doc:  "checks cohesion",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	fmt.Println("Hello, world!")
+	return nil, nil
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	singlechecker.Main(analyzer)
 }
