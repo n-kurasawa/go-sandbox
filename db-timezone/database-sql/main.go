7300f0a12317bc8067cf065774a4b02348e22d9d
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2022-08-21 00:52:18 +0900

    refactor

--- db-timezone/database-sql/main.go
+++ db-timezone/database-sql/main.go
@@ -23,8 +23,8 @@ func main() {
 
 	// [1] UTC の 2022-08-20 00:00:00 で検索
 	utc, _ := time.LoadLocation("UTC")
-	utcDatetime := time.Date(2022, 8, 20, 0, 0, 0, 0, utc)
-	if result, err := findByDatetime(db, utcDatetime); err == nil {
+	utcTime := time.Date(2022, 8, 20, 0, 0, 0, 0, utc)
+	if result, err := findByDatetime(db, utcTime); err == nil {
 		fmt.Printf("[1] id: %d, datetime: %v\n", result.id, result.datetime)
 	} else {
 		fmt.Println("[1]", err)
@@ -32,24 +32,24 @@ func main() {
 
 	// [2] JST の 2022-08-20 00:00:00 で検索
 	jst, _ := time.LoadLocation("Asia/Tokyo")
-	jstDatetime := time.Date(2022, 8, 20, 0, 0, 0, 0, jst)
-	if result, err := findByDatetime(db, jstDatetime); err == nil {
+	jstTime := time.Date(2022, 8, 20, 0, 0, 0, 0, jst)
+	if result, err := findByDatetime(db, jstTime); err == nil {
 		fmt.Printf("[2] id: %d, datetime: %v\n", result.id, result.datetime)
 	} else {
 		fmt.Println("[2]", err)
 	}
 
 	// [3] UTC の 2022-08-20 00:00:00 を JST にして検索
-	if result, err := findByDatetime(db, utcDatetime.In(jst)); err == nil {
+	if result, err := findByDatetime(db, utcTime.In(jst)); err == nil {
 		fmt.Printf("[3] id: %d, datetime: %v\n", result.id, result.datetime)
 	} else {
 		fmt.Println("[3]", err)
 	}
 }
 
-func findByDatetime(db *sql.DB, dateTime time.Time) (sample, error) {
+func findByDatetime(db *sql.DB, datetime time.Time) (sample, error) {
 	var result sample
-	if err := db.QueryRow("select id, datetime from sample_table where datetime = ?", dateTime).Scan(&result.id, &result.datetime); err != nil {
+	if err := db.QueryRow("select id, datetime from sample_table where datetime = ?", datetime).Scan(&result.id, &result.datetime); err != nil {
 		return sample{}, fmt.Errorf("failed QueryRow: %w", err)
 	}
 	return result, nil
